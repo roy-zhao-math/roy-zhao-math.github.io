f4e6cadaf0119294ae9c1a01d56666f9eeea56f9
ROY-LAPTOP\Roy <roy.h.zhao@gmail.com>
2024-09-30 17:31:38 -0700

    Import website from Caltech

---
+++ header.js
@@ -0,0 +1,23 @@
+document.write('\
+\
+    <div id="header">\
+      <table width="100%">\
+	<tbody>\
+	  <tr>\
+	    <td>\
+	      <div id="name"><a href="/~rhzhao/index.html" style="text-decoration: none; color: inherit;">Roy Zhao</a>\
+	      </div>\
+	    </td>\
+	    <td align="right" valign="middle">\
+	      <div class="navbar">\
+		<a class="header" href="/~rhzhao/index.html">About</a>\
+		<a class="header" href="/~rhzhao/teaching.html">Teaching</a>\
+		<a class="header" href="/~rhzhao/service.html">Service</a>\
+<!--		<a class="header" href="/~rhzhao/cv.html">CV</a>\
+-->	      </div>\
+	    </td>\
+	  </tr>\
+	</tbody>\
+      </table>\
+    </div>\
+');
